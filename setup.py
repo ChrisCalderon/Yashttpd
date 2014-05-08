7ba38be99573698b8989f39b4e93f038f011742c
Christian Calderon <calderon.christian760@gmail.com>
2014-05-07 17:37:04 -0700

    Beginning switch to non-blocking sockets

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="Yashttpd",
+      version="0.0.a0",
+      description="Yet Another Super-cool HTTP Daemon",
+      author="Christian Calderon",
+      author_email="calderon.christian760@gmail.com",
+      packages=["yashttpd"]
+)
--- yashttpd/yashttpd.py
+++ yashttpd/yashttpd.py
@@ -1,22 +1,84 @@
-import socket
+###############################################################################
+from socket import socket, SOL_SOCKET, SO_REUSEADDR, error as sock_err
+from errno import EAGAIN, EWOULDBLOCK
+from sys import stdout, exit
 from string import strip
 from json import dumps as package
 from constants import HTTP_VERS, HTTP_CODES
-from sys import stdout
+from select import select
 
-def make_server(ip, port, conq):
-    s = socket.socket()
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+SMALLEST_CHUNK = 4096
+
+def make_server(ip, port, conq, blocking=True, verbose=True):
+    """Make a socket for the server."""
+    s = socket()
+    if not blocking:
+        #I must remember to set every client socket to non-blocking
+        #The following two comments are not true!
+        #Making the server socket non-blocking makes 
+        #all the clients non-blocking too!
+        s.setblocking(0)
+    s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     s.bind((ip, port))
     s.listen(conq)
-    print "socket #%d listening @ %s:%d" % (s.fileno(), ip, port)
+    if verbose:
+        print "socket #%d listening @ %s:%d" % (s.fileno(), ip, port)
     return s
 
 def pretty(dic):
+    """Pretty print a json-compatible dictionary."""
     print package(dic, sort_keys=True, indent=4)
 
+def get_message(client, chunk):
+    #We must have a integer for the maximum bytes to read,
+    assert(type(chunk)==int) 
+    #and that integer must be reasonably large.
+    assert(chunk >= SMALLEST_CHUNK)
+    #print "recving request"
+
+    #try to get data from the socket
+    check = True
+    while check:
+        try:
+            message = client.recv(chunk)
+        except sock_err, e:
+            err = e.args[0]
+            if err != EAGAIN and err != EWOULDBLOCK:
+                print err
+                print e
+                exit(1)
+        else:
+            check = False
+
+    #If there is still data on the socket after reading chunk bytes,
+    #then the sender sent too much data. In order to make sure we read
+    #all the bytes, we try to read another byte. if we can, that is bad.
+    #If we can't and it is because of EAGAIN or EWOULDBLOCK, then we are
+    #golden! If we can't and it isn't for the expected reason, everything 
+    #dies!
+    try:
+        #print 'checking for extra bytes'
+        extra = client.recv(1)
+    except sock_err, e:
+        #print 'error on recv caught! (good news)'
+        err = e.args[0]
+        if err != EAGAIN and err != EWOULDBLOCK:
+            print err
+            print e
+            exit(1)
+        return message
+    else:
+        #print "too many bytes in request!"
+        #print message
+        return {'code':'413'} #request too large, bad!
+
+
 def parse_request(client, address, chunk):
-    message = client.recv(chunk)
+    """Parses the http request from the client into a dictionary.
+    The entire request must fit into a string of chunk bytes."""
+    message = get_message(client, chunk)
+    if type(message) == dict:
+            return message
     request, message = message.split("\r\n", 1)
     request = request.split(' ', 3)
     headers, message = message.split("\r\n\r\n", 1)
@@ -35,19 +97,25 @@ def send_response(client, address, response_dict):
     code = response_dict['code']
     code_msg = HTTP_CODES[int(code)][0]
     response = HTTP_VERS + ' ' +  code + ' ' + code_msg + '\r\n'
-    client.send(response)
-    headers = '\r\n'.join(': '.join(item) for item in response_dict.get('headers', {}).items())
-    client.send(headers + '\r\n\r\n')
-    client.send(response_dict.get('message', ''))
+    items = (i for i in response_dict.get('headers',{}).items())
+    headers = '\r\n'.join(': '.join(i) for i in items)
+    #Aparently i only get one send with non blocking sockets.
+    client.send(response+headers+'\r\n\r\n'+response_dict.get('message',''))
 
 def serve_forever(ip, port, conq, chunk, handler):
-        s = make_server(ip, port, conq)
+        s = make_server(ip, port, conq, blocking=False)
         try:
             while True:
+                readable, writeable, errors_maybe = select([s],[],[])
                 c, a = s.accept()
+                c.setblocking(0)
                 request_dict = parse_request(c, a, chunk)
-                response_dict = handler(request_dict)
-                send_response(c, a, response_dict)
+                #If the request dict only has 1 key, it is an error code
+                if len(request_dict) == 1:
+                    send_response(c, a, request_dict)
+                else:
+                    response_dict = handler(request_dict)
+                    send_response(c, a, response_dict)
                 c.close()
         except KeyboardInterrupt:
             stdout.write("\r")
@@ -67,8 +135,17 @@ if __name__ == "__main__":
         #Normally, you would actually look at the contents of
         #request_dict and determine create your response_dict
         #from that. This is just a demo.
+        if request_dict['uri'] != '/':
+            return {'code':'404'}
         html = '<html><body><h1>Hello World!</h1></body></html>'
         typ = 'text/html'
-        return {'code':'200', 'message':html, 'headers':{'Content-Type':typ, 'Content-Length':str(len(html))}}
+        return {
+            'code':'200', 
+            'message':html, 
+            'headers':{
+                'Content-Type':typ, 
+                'Content-Length':str(len(html))
+            }
+        }
 
     serve_forever(IP, PORT, CONQ, CHUNK, handler)
